d9cce40dba870adf1699fa7a6d799705fc26b7a5
Linus Styrén <linus.styren@aptiv.com>
2020-11-17 14:51:21 +0100

    use newlines

--- ifttt.sh
+++ ifttt.sh
@@ -3,4 +3,7 @@
 TOKEN="$1"
 CHANNEL="$2"
 
-curl -X POST -H 'Content-type: application/x-www-form-urlencoded' --data 'token='"$TOKEN"'&channel='"$CHANNEL"'&username=IFTTT-ersättare-slackboten&text=Vad har du gjort under dagen?\nVad har du lärt dig?\nVad förstod du inte / Vilka frågor har du inte fått svar på?\nVad vill du lära dig mer om? :memo:' https://slack.com/api/chat.postMessage
+curl -X POST -H 'Content-type: application/x-www-form-urlencoded' --data 'token='"$TOKEN"'&channel='"$CHANNEL"'&username=IFTTT-ersättare-slackboten&text=Vad har du gjort under dagen?
+Vad har du lärt dig?
+Vad förstod du inte / Vilka frågor har du inte fått svar på?
+Vad vill du lära dig mer om? :memo:' https://slack.com/api/chat.postMessage
